80ae1d7b21b35a0ede3d1e2e6eab9569f5ca13c8
Thanan Sapthamrong <thanan.s@coraline.co.th>
2020-01-06 00:08:18 +0700

    add multi as a parameter to determine the   query execution (single statement or multiple statements)

--- coralinedb/coralinedb.py
+++ coralinedb/coralinedb.py
@@ -242,20 +242,21 @@ class BaseDB:
 
         return result
 
-    def execute(self, sql_statement, db_name=None, params=None):
+    def execute(self, sql_statement, db_name=None, params=None, multi=False):
         """
         Execute SQL Statement to database
         :param sql_statement: sql statement (str)
         :param db_name: database name (str)
-        :param params: parameters to cast the %s in sql statement (tuple or dictionary), for usecase read the link below
+        :param params: cast the %s in sql statement (tuple or dictionary), for usecase read the link below
                     https://dev.mysql.com/doc/connector-python/en/connector-python-api-mysqlcursor-execute.html
+        :param multi: is able to execute multiple sql statement within one execution (bool)
         :return:
         """
         # Create Connection
         engine, connection = self.create_connection(db_name)
 
         # Execute SQL
-        result = connection.execute(sql_statement, params)
+        result = connection.execute(sql_statement, params, multi)
 
         # Close connection
         connection.close()
