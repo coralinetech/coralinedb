cd6c8c415fc9d298dca5656d568898721212d025
Jiranun Jiratrakanvong <jiranun@coraline.co.th>
2020-04-29 10:27:51 +0700

    workaround fix for postgresql v. 2.10.1

--- coralinedb/coraline_postgresql.py
+++ coralinedb/coraline_postgresql.py
@@ -26,3 +26,35 @@ class PostgreSQLDB(BaseDB):
 
         return f"postgresql://{self.username}:{self.passwd}@{self.host}:{self.port}/{db_name}"
 
+
+    def load_table(self, db_name: str, table_name: str) -> pd.DataFrame:
+        """Load a table from database
+
+        Parameters
+        ----------
+        db_name : str
+            database name
+        table_name : str
+            table name
+
+        Returns
+        -------
+        pd.DataFrame
+            loaded table
+        """
+
+        # Create Connection
+        _, connection = self.create_connection(db_name)
+
+        # Check if table exists and read
+        try:
+            sql = 'SELECT * FROM %s' % table_name
+            result = pd.read_sql(sql, connection, coerce_float=True)
+        except Exception as e:
+            print(e)
+            result = None
+
+        # Close connection
+        connection.close()
+
+        return result
